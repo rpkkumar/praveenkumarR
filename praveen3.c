f5ca9705e9bda5cd023ce9d4b7e206e79363ec04
rpkkumar <35652674+rpkkumar@users.noreply.github.com>
2018-01-30 16:26:05 +0530

    Rename praveen2.c to praveen3.c

---
+++ praveen3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char n;
+clrscr();
+printf("enter the character");
+scanf("%c",&n);
+if((n==a||n==e||n==i||n==o||n==u)
+{
+printf("the character is vowel");
+}
+else
+{
+printf("the character is consonent");
+}
+getch();
+}
