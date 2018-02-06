1344717bf463c6b3d87f1c196ef23f0968f1fa89
rpkkumar <35652674+rpkkumar@users.noreply.github.com>
2018-02-06 15:54:39 +0530

    Create praveen7.c

---
+++ praveen7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+ {
+
+	int a;
+    printf("enter the number");
+  scanf("%d",&a);
+   if(a%4==0)
+   {
+  printf("this year is leap year");
+   }
+  else
+  {
+  printf("this year is not leap year");
+  }
+	return 0;
+}
