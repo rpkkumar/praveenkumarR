af04259913051b4757a49abd80a4ebabb5202d16
rpkkumar <35652674+rpkkumar@users.noreply.github.com>
2018-02-06 16:02:28 +0530

    Create praveen9.c

---
+++ praveen9.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,i;
+    clrscr();
+    printf("Enter the N value:");
+    scanf("%d",&a);
+    i=(a*(a+1)/2);
+        printf("%d\n",i);
+    getch();
+}
