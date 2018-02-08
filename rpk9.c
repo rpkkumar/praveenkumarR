f7f1de917334779e5f2758f39e163678a918061e
rpkkumar <35652674+rpkkumar@users.noreply.github.com>
2018-02-08 16:53:23 +0530

    Create rpk9.c

---
+++ rpk9.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int s,p,i ,sum=0;
+  scanf(" %d%d", &s &p");
+  printf("%d%d",s,p);
+  for(i=0;i<n,i++)
+  {
+    printf("%d",b[i]);
+  }
+  for(i=0;i<=p;i++)
+  { 
+  sum=sum+b[i];
+  }
+  printf(" \s %d",sum);
+}  
+
