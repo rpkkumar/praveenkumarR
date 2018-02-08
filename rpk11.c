a7212081235701c4fa0181013118361466913c7c
rpkkumar <35652674+rpkkumar@users.noreply.github.com>
2018-02-08 16:56:41 +0530

    Create rpk11.c

---
+++ rpk11.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main ()
+{
+    int a,r,s;
+    
+    printf("enter the numbers:");
+    scanf("%d",&a);
+    printf("the power value:");
+    scanf("%d",&s);
+    b= pow(a,s);
+    printf(" the power value is %d^%d=%d",a,s,r);
+    
+}
