b56af572eb1c8f7f0cf10cf4143807f817a69507
rpkkumar <35652674+rpkkumar@users.noreply.github.com>
2018-03-01 15:49:45 +0530

    Create praveen19.c

---
+++ praveen19.c
@@ -0,0 +1,18 @@
+
+include<stdio.h>
+#include<conio.h>
+void main()
+{
+int s,fact=1,i;
+printf("enter the value");
+scanf("%d",&s);
+for(i=1;i<=n;i++)
+{
+fact=fact*i;
+}
+printf("%d",fact);
+getch();
+}
+
+
+
