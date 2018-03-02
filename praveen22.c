057656b810a33cc0cdf1eb69dce9b8d0bde9451a
rpkkumar <35652674+rpkkumar@users.noreply.github.com>
2018-03-02 14:33:51 +0530

    Create praveen22.c

---
+++ praveen22.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[50],i,n,max;
+printf("enter the range");
+scanf("%d",&n);
+printf("enter the array values");
+for(i=0;i<n;i++)
+{
+scanf("%d",&b[i]);
+}
+max=b[0];
+for(i=0;i<n;i++)
+{
+if(b[i]>max)
+{
+max=b[i];
+}
+}
+printf("%d",max);
+getch();
+}
