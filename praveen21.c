2e3c5357e1723f9c7c06d01554d6187f16a72bc5
rpkkumar <35652674+rpkkumar@users.noreply.github.com>
2018-03-02 14:24:50 +0530

    Rename praveen20.c to praveen21.c

---
+++ praveen21.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,a,d,s,ans=0,i;
+scanf("%d",&n);
+scanf("%d",&a);
+scanf("%d",&d);
+for(j=1;j<=n;j++)
+s=a+(i-1)*d;
+ans=ans+s;
+}
+printf("%d",ans);
+return 0;
+}
