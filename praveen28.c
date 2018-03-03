72fded18fa9e8552c8c50ffa737d4f77a583caa6
rpkkumar <35652674+rpkkumar@users.noreply.github.com>
2018-03-03 14:53:14 +0530

    Create praveen28.c

---
+++ praveen28.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) 
+{
+int arr[20],n;
+scanf("%d\n",&n);
+for(int i=0;i<n;i++)
+{
+scanf("%d\n",&arr[i]);
+}
+for(int i=0;i<n;i++)
+{
+printf("%d %d\n",arr[i],i);
+}
+return 0;
+}
