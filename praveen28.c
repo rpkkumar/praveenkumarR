16f1f662ae7b72522e6a6912bbc91d92ae5699e0
rpkkumar <35652674+rpkkumar@users.noreply.github.com>
2018-03-03 14:56:16 +0530

    Create praveen28.c

---
+++ praveen28.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+    int n,a[10],i;
+    scanf("%d",&s);
+    for(i=0;i<s;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<s;i++)
+    {
+      printf("%d %d\s",a[i],i);
+    }
+	return 0;
+}
