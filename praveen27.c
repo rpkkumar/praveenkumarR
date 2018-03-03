ad8fadd4cc12b161e255c400309185afb2f441cb
rpkkumar <35652674+rpkkumar@users.noreply.github.com>
2018-03-03 14:49:00 +0530

    Create praveen27.c

---
+++ praveen27.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+	int k,x;
+	char s[100];
+	scanf("%s",s);
+	for(k=0;s[k]!=0;++k)
+	{
+		
+	}
+	for(int i=0;i<k;i++)
+	{
+		x=isdigit(s[i]);
+		
+	}
+	if(x!=0)
+	printf("Yes");
+	else
+	printf("No");
+	return 0;
+}
