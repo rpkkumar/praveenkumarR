470d2ff36ff78cd6b82afbf8cd7fad31baaa585e
rpkkumar <35652674+rpkkumar@users.noreply.github.com>
2018-03-03 14:42:55 +0530

    Create praveen25.c

---
+++ praveen25.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[10],i,n,k,sum=0;
+    printf("enter the limit");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+    }
+    k=sum/n;
+    printf("%d",k);
+    getch();
+}
