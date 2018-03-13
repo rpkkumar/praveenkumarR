5b97eb3185ce6aacbb01a3d78e8a0e93aee4fde0
rpkkumar <35652674+rpkkumar@users.noreply.github.com>
2018-03-13 16:15:35 +0530

    Create praveen34.c

---
+++ praveen34.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	char s[50];
+	int i,count=1;
+	gets(s);
+	
+	for(i=0;s[i]!='\0';i++)
+	{
+		if(s[i] == '.')
+		count++;
+		
+		
+	}
+	printf("%d",count);
+	return 0;
+}
