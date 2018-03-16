a62c51d895ac440f133b8fe9840c54c7f43013e7
rpkkumar <35652674+rpkkumar@users.noreply.github.com>
2018-03-16 14:42:59 +0530

    Create praveen48.c

---
+++ praveen48.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+   int s,a[50],i=0,sum=0;
+printf("enter the limit");
+scanf("%d",&s);
+printf("enter array element");
+for(i=0;i<s;i++)
+{
+scanf("%d",&a[i]);
+sum=sum+a[i];
+}
+printf("average = %d",(sum/n));
+
+    return 0;
+}
