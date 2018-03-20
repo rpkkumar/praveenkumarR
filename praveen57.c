4f8591ce2dbf8b07c39c566ca883598985d169d4
rpkkumar <35652674+rpkkumar@users.noreply.github.com>
2018-03-20 16:18:02 +0530

    Create praveen57.c

---
+++ praveen57.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+   int z,y,t;
+   scanf("%d %d",&z,&y);
+   if(a!=0)
+   {
+       t=z;
+       z=y;
+       y=t;
+   }
+   printf("%d %d",z,y);
+    return 0;
+}
